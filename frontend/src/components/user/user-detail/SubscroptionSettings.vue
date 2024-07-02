8e0d3d08130945b2a9731b76c5cb0ecd5e1aa699
정민균 <50010672+mingyun-jeong@users.noreply.github.com>
2024-07-02 22:22:04 +0900

    Subscription 페이지 제작 중

--- frontend/src/components/user/user-detail/SubscroptionSettings.vue
+++ frontend/src/components/user/user-detail/SubscroptionSettings.vue
@@ -1,23 +1,95 @@
 <script lang="ts" setup>
 
 const selectedPlan = ref('Creator')
+let msTts = ref(false)
+let waterMark = ref(false)
+let fhd = ref(false)
+let uhd = ref(false)
 
 </script>
-
 <template>
-  <VCard title="Current Plan">
+  <VCard title="Subscription">
     <VCardText>
       <VForm @submit.prevent="() => {}">
+        <VRow>
+          <VCol cols="12">
+            <VLabel>Current Plan</VLabel>
+          </VCol>
+        </VRow>
         <VRow>
           <VCol cols="6" sm="2">
             <VSelect v-model="selectedPlan" mandatory :items="['Free', 'Creator']"/>
           </VCol>
         </VRow>
+
         <VRow>
           <VCol cols="12">
+            <VLabel>Plan Options</VLabel>
+          </VCol>
+        </VRow>
+        <VRow>
+          <VCol cols="1">
             <VSwitch v-model="msTts">
               <template #label>MS-TTS</template>
             </VSwitch>
+          </VCol>
+          <VCol cols="1">
+            <VSwitch v-model="waterMark">
+              <template #label>워터마크</template>
+            </VSwitch>
+          </VCol>
+        </VRow>
+
+        <VRow>
+          <VCol cols="4">
+            <VTextField label="프로젝트 생성 개수"/>
+          </VCol>
+          <VCol cols="4">
+            <VTextField label="영상 내보내기 시간(분)"/>
+          </VCol>
+          <VCol cols="4">
+            <VTextField label="저장 용량(MB)"/>
+          </VCol>
+          <VCol cols="4">
+            <VTextField label="업로드 용량 제한(MB)"/>
+          </VCol>
+          <VCol cols="4">
+            <VTextField label="초대 가능 계정(명)"/>
+          </VCol>
+        </VRow>
+        <VRow>
+          <VCol cols="12">
+            <VLabel>Video Quality</VLabel>
+          </VCol>
+        </VRow>
+        <VRow>
+          <VCol cols="1">
+            <VCheckbox v-model="fhd">
+              <template #label>FHD</template>
+            </VCheckbox>
+          </VCol>
+          <VCol cols="1">
+            <VCheckbox v-model="uhd">
+              <template #label>UHD</template>
+            </VCheckbox>
+          </VCol>
+        </VRow>
+
+        <VRow>
+          <VCol cols="12">
+            <VLabel>Available Model</VLabel>
+          </VCol>
+        </VRow>
+        <VRow>
+          <VCol cols="1">
+            <VCheckbox>
+              <template #label>큐리</template>
+            </VCheckbox>
+          </VCol>
+          <VCol cols="1">
+            <VCheckbox>
+              <template #label>나탈리</template>
+            </VCheckbox>
           </VCol>
         </VRow>
 
